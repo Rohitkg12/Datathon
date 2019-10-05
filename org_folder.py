17c7b9e67f087b60d08f0774724a08b125d366bf
Saket Khandelwal <saketdecade17@gmail.com>
2019-10-05 20:35:04 +1000

    Added organization script

---
+++ org_folder.py
@@ -0,0 +1,38 @@
+'''
+Steps:
+
+1. Extract dataset to a folder.
+2. Move this script into that folder and run it (Should be at the same level as 'Phase02-DataDelivery').
+
+'''
+
+
+import os
+import shutil
+import re
+
+try:
+    os.mkdir('data')
+except Exception:
+    print('DIR exists')
+
+
+masks = os.listdir('Phase02-DataDelivery/masks')
+metadata = os.listdir('Phase02-DataDelivery/metadata')
+dates = [os.path.splitext(data)[0] for data in metadata]
+
+for mask in masks:
+    mask = os.path.splitext(mask)[0]
+    try:
+        os.mkdir(f'data/{mask}')
+    except Exception:
+        pass
+    
+    search = '-'.join(re.findall('\d+',mask))
+    photos = os.listdir('Phase02-DataDelivery/sugarcanetiles')
+    for date in dates:
+        os.mkdir(f'data/{mask}/{date}')
+        shutil.copy2(f'Phase02-DataDelivery/metadata/{date}.json',f'data/{mask}/{date}')
+        for photo in photos:
+            if search in photo and date in photo:
+                shutil.copy2(f'Phase02-DataDelivery/sugarcanetiles/{photo}',f'data/{mask}/{date}')
