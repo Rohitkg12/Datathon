53f1e1dff26fdf5a0ae48d1f24ce541c5f449135
abhinavcreed13 <abhinavcs13@gmail.com>
2019-10-07 15:32:49 +1100

    Adding initial web-app

---
+++ react-web-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Header from './components/header/header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-web-app/src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+            Project Satellite
+        </nav>
+    )
+};
+
+export default Header;
